bf26ea964079dbaf38af1c3deaf5a6de7e9ba37b
Austin Anderson <Austin.David.Anderson1@gmail.com>
2018-05-04 19:21:07 -0500

    game generation almost done, but one or two problem cases

--- gameGeneration/java/RandomGuessBFSCheck/src/Board.java
+++ gameGeneration/java/RandomGuessBFSCheck/src/Board.java
@@ -11,8 +11,19 @@ public class Board {
 		//one in iceOdds chance of being rock
 		return ((int)(Math.random()*iceOdds))%iceOdds==0;
 	}
-	private final Position start;
-	private final Position end;
+	private int getRandNonRockColNdx(int row){
+		int[] ndxs=new int[width];
+		int maxIndex=0;
+		for(int i=0;i<width;i++){
+			if(!board[row][i].rock){
+				ndxs[maxIndex]=i;
+				maxIndex++;
+			}
+		}
+		return ndxs[((int)(Math.random()*maxIndex))%maxIndex];
+	}
+	private Position start;
+	private Position end;
 	public Board(boolean[][] toSolve){
 		height=toSolve.length;
 		width=toSolve[0].length;
@@ -29,81 +40,96 @@ public class Board {
 		start=startValue;
 		end=endValue;
 	}
-	public Board(int height,int width){
-		this.height=height;
-		this.width=width;
-		board=new Position[height][width];
-		for(int r=0;r<height;r++){
-			for(int c=0;c<width;c++){
-				boolean rock=true;
-				if(r!=0&&r!=height-1&&c!=0&&c!=width-1) rock=randRock();
-				board[r][c]=new Position(r,c,rock);
-			}
+	public Board(int height,int width,int minMoves){
+		if(height<3) height=3;
+		int solutionLength=0;
+        this.height=height;
+        this.width=width;
+		while(solutionLength<minMoves){
+            board=new Position[height][width];
+            for(int r=0;r<height;r++){
+                for(int c=0;c<width;c++){
+                    boolean rock=true;
+                    if(r!=0&&r!=height-1&&c!=0&&c!=width-1) rock=randRock();
+                    board[r][c]=new Position(r,c,rock);
+                }
+            }
+            
+            
+            start=board[0][getRandNonRockColNdx(1)];
+            int endCol=getRandNonRockColNdx(height-2);
+            board[height-1][endCol]=new Position(height-1,endCol,false);
+            end=board[height-1][endCol];
+            String result=solve();
+            if(result!=null) solutionLength=result.length();
 		}
-		start=null;
-		end=null;
 	}
 	private Position[][] board;
 	private class Position{
 		private final static char UP='^',DOWN='v',LEFT='<',RIGHT='>',DONE='e';
-		public final static int StateCount=4;
+		public final static int NumStates=5;
 		public Position(int r,int c,boolean isRock){
 			this.r=r;
 			this.c=c;
 			this.rock=isRock;
 		}
-		public String getPath(){
-			return path;
-		}
 		public final int r;
 		public final int c;
 		public final boolean rock;
-		private boolean visited=false;
-		private String path="";
 		private char state=UP;
+		private String path="";
+		public String getPath(){
+			return path;
+		}
+		private boolean visited=false;
+		public void setVisited(){
+			visited=true;
+		}
+		private boolean isOutOfBoundsOrRock(int r,int c){
+            return r<0||r>=board.length||
+                   c<0||c>=board[0].length||
+                   board[r][c].rock;
+		}
 		public Position next(){
 			Position toReturn=null;
-			0nji9
-			
 			if(state!=DONE){
                 int otherR=r;
                 int otherC=c;
                 int modr=0;
                 int modc=0;
-                char direction=state;
-                     if(state==   UP){state=LEFT;  modr=-1;}
-                else if(state== LEFT){state=DOWN;  modc=-1;}
-                else if(state== DOWN){state=RIGHT; modr=1; }
-                else if(state==RIGHT){state=DONE;  modc=1; }
-                while(!board[otherR+modr][otherC+modc].rock&&board[otherR][otherC]!=end){
+                char direction=state; 
+                
+                     if(state==   UP){ state=DOWN;modr=-1;}
+                else if(state== DOWN){ state=LEFT;modr= 1;}
+                else if(state== LEFT){state=RIGHT;modc=-1;}
+                else if(state==RIGHT){ state=DONE;modc= 1;}
+                while(!isOutOfBoundsOrRock(otherR+modr,otherC+modc)&&board[otherR][otherC]!=end){
                     otherR+=modr;
-                    otherC+=modr;
+                    otherC+=modc;
                 }
-                toReturn=board[otherR][otherC];
-                if(toReturn.visited){
-                	toReturn=null;
-                }else{
-                	toReturn.visited=true;
-                	toReturn.path=this.path+direction;
+                if(!board[otherR][otherC].visited){
+                    toReturn=board[otherR][otherC];
+                    toReturn.path=this.path+direction;
                 }
 			}
-			
 			return toReturn;
 		}
 		public void print(PrintStream printer) {
-			           char out='+';
-			if(rock)        out='@';
+			           char out='.';
+			if(rock)        out='O';
 			if(this==start) out='s';
 			if(this==end)   out='e';
 			printer.print(out);
 		}
-		@Override
-		public String toString(){
-			return String.format("{(%d,%d) %s, %s}",r,c,state,path);
-		}
 	}
 	public void print(PrintStream printer){
+	    printer.print(' ');
+	    for(int c=0;c<width;c++){
+	        printer.print(Integer.toHexString(c));
+	    }
+	    printer.println();
 		for(int r=0;r<height;r++){
+	        printer.print(Integer.toHexString(r));
 			for(int c=0;c<width;c++){
 				board[r][c].print(printer);
 			}
@@ -113,61 +139,85 @@ public class Board {
 	public String solve(){
 		Queue<Position> queue=new LinkedList<Position>();
 		queue.add(start);
-		while(!queue.isEmpty()&&queue.peek()!=end){
-			Position head=queue.remove();
-			for(int i=0;i<Position.StateCount;i++){
+		start.setVisited();
+		Position head=null;
+		while(head!=end&&!queue.isEmpty()){
+			head=queue.remove();
+			for(int i=0;i<Position.NumStates;i++){
 				Position next=head.next();
-				if(next!=null) {
+				if(next!=null){
 					queue.add(next);
+					next.setVisited();
 				}
 			}
 		}
-		String result=null;
-		if(!queue.isEmpty()) result=queue.poll().getPath();//if !empty then should be end position
-		return result;
+		String solvePath=null;
+		if(head==end){
+			solvePath=head.getPath();
+		}
+		return solvePath;
 	}
 	public static void main(String[] args){
-		final boolean XX=true;
-		final boolean __=false;
-		final boolean  o=false;
-		final boolean oo=false;
-		final boolean oO=false;
-		final boolean Oo=false;
-		final boolean  O=false;
-		Board unwinnable=new Board(new boolean[][]{
-			{XX, O,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX},
-			{XX, O, o,oo,oo,oo,oo,oo,oo,oo,oo,XX,__,XX},
-			{XX, O,oO,XX,__,__,__,__,__,__, O,__,__,XX},
-			{XX,XX,__,__,__,XX,oo,oo,oo,oo,oO,__,__,XX},
-			{XX,__,__,__,__,__,O ,__,__,__,XX,__,__,XX},
-			{XX,__,__,__,__,__,Oo,oo,oo,oo,oo,oo,XX,XX},
-			{XX,__,__,__,__,__,XX,__,__,__,__,XX,__,XX},
-			{XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,__,XX,XX},
+		final boolean O=true;
+		final boolean _=false;
+		final boolean $=false;
+		/*
+		Board b1=new Board(new boolean[][]{
+			{O,$,O,O,O,O,O,O,O,O,O,O,O,O},
+			{O,$,$,$,$,$,$,$,$,$,$,O,_,O},
+			{O,$,$,O,_,_,_,_,_,_,$,_,_,O},
+			{O,O,_,_,_,O,$,$,$,$,$,_,_,O},
+			{O,_,_,_,_,_,$,_,_,_,O,_,_,O},
+			{O,_,_,_,_,_,$,$,$,$,$,$,O,O},
+			{O,_,_,_,_,_,O,_,_,_,_,$,_,O},
+			{O,O,O,O,O,O,O,O,O,O,O,$,O,O},
 		});
-		Board b1=new Board(new boolean[][]{//v>^>v<v>v
-			{XX,O ,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX},
-			{XX,O , o,oo,oo,oo,oo,oo,oo,oo,oo,XX,__,XX},
-			{XX,Oo,oO,XX,__,__,__,__,__,__, O,__,__,XX},
-			{XX,XX,__,__,__,XX,oo,oo,oo,oo,oO,__,__,XX},
-			{XX,__,__,__,__,__,O ,__,__,__,XX,__,__,XX},
-			{XX,__,__,__,__,__,Oo,oo,oo,oo,oo,oo,XX,XX},
-			{XX,__,__,__,__,__,XX,__,__,__,__, O,__,XX},
-			{XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX, O,XX,XX},
+		Board b2=new Board(new boolean[][]{
+			{O,O,O,O,O,O,O,O,O,O,O,O,$,O,O},
+			{O,_,_,_,_,_,_,_,_,_,_,_,$,_,O},
+			{O,_,_,_,O,$,$,$,$,$,$,$,$,_,O},
+			{O,_,_,_,_,$,_,_,_,_,_,_,O,_,O},
+			{O,_,_,O,_,$,_,_,_,_,_,_,_,_,O},
+			{O,_,_,$,$,$,$,$,$,$,$,O,_,_,O},
+			{O,_,_,$,_,$,_,_,_,_,$,_,_,_,O},
+			{O,_,O,$,$,$,O,$,$,$,$,_,_,_,O},
+			{O,_,_,_,_,O,_,$,_,_,O,_,_,_,O},
+			{O,O,O,O,O,O,O,$,O,O,O,O,O,O,O},
 		});
-		Board b2=new Board(new boolean[][]{//v<v<^>v<v
-			{XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,XX,O ,XX,XX},
-			{XX,__,__,__,__,__,__,__,__,__,__,__,O ,__,XX},
-			{XX,__,__,__,XX,oo,oo,oo,oo,oo,oo,oo,O ,__,XX},
-			{XX,__,__,__,__,O ,__,__,__,__,__,__,XX,__,XX},
-			{XX,__,__,XX,__,O ,__,__,__,__,__,__,__,__,XX},
-			{XX,__,__,oo,oo,Oo,oo,oo,oo,oo,oo,XX,__,__,XX},
-			{XX,__,__,O ,__,O ,__,__,__,__, O,__,__,__,XX},
-			{XX,__,XX,Oo,oo,O ,XX,oo,oo,oo,oO,__,__,__,XX},
-			{XX,__,__,__,__,XX,__,O ,__,__,XX,__,__,__,XX},
-			{XX,XX,XX,XX,XX,XX,XX,O ,XX,XX,XX,XX,XX,XX,XX},
+		Board b3=new Board(new boolean[][]{
+			{O,O,O,_,O},
+			{O,_,_,_,O},
+			{O,O,_,_,O},
+			{O,O,_,_,O},
+			{O,_,O,O,O},
+		});  
+		Board b4=new Board(new boolean[][]{
+			{O,O,O,$,O,O,O,O,O,O},
+			{O,_,O,_,_,_,_,_,_,O},
+			{O,_,_,O,_,_,_,O,_,O},
+			{O,O,_,_,_,O,_,_,_,O},
+			{O,O,O,O,O,O,$,O,O,O}
 		});
+		*/
+		//b2.print(System.out);
+		//System.out.println(b2.solve());
 		//b1.print(System.out);
-		b2.print(System.out);
-		System.out.println(b2.solve());
+		//System.out.println(b1.solve());
+		new Board(10,10,8).print(System.out);
+		/*
+		
+		failed on this one
+0123456789
+0OOOOOOsOOO
+1O........O
+2O...O....O
+3OO...O..OO
+4OO...O...O
+5O...O....O
+6O.......OO
+7O..OO..O.O
+8O...OO...O
+9OeOOOOOOOO
+		*/
 	}
 }
